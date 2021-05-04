4c1f4fbae41b0290d2d7dbd68e30446ea06bbf70
Corrlie <wojciech.konkol1@gmail.com>
2021-05-04 10:20:46 +0200

    Added game file

---
+++ ros_game.py
@@ -0,0 +1,75 @@
+from Player import PlayerPoint
+from Turtle import TurtlePosition
+from NodeMgmt import *
+import time
+
+
+def calc_to_turtle_plane(raw_x, raw_y):
+    rescaled_x = raw_x/46
+    rescaled_y = raw_y/46
+    return rescaled_x, rescaled_y
+
+player1 = PlayerPoint()
+print('Player 1')
+print('Choose position of a goal point')
+(raw_goal_x1, raw_goal_y1) = player1.get_coords()
+
+(scaled_goal_x1, scaled_goal_y1) = calc_to_turtle_plane(raw_goal_x1, raw_goal_y1)
+
+turtle1 = TurtlePosition()
+
+process_window = run_node('turtlesim', 'turtlesim_node')
+process_teleop = run_node('turtlesim', 'turtle_teleop_key')
+start_timer = time.time()
+print('Timer started!')
+distance_state = 'Distance unknown'
+new_distance_state = 'Distance unknown'
+while(1):
+    distance = turtle1.calc_distance(turtle1.get_pose_x(), turtle1.get_pose_y(), scaled_goal_x1, scaled_goal_y1)
+    # print('Distance: ')
+    # print(distance)
+    if (distance>8):
+        distance_state = 'REALLY COLD!'
+    elif(distance<=8 and distance > 4):
+        distance_state = 'cold'
+    elif(distance<=4 and distance>2):
+        distance_state='warm'
+    elif(distance<=2 and distance>0.2):
+        distance_state = 'HOT'
+    else:
+        distance_state = 'POINT FOUND'
+        print('CONGRATS! YOU FOUND THE POINT')
+        # close_node(process_window)
+        # close_node(process_teleop)
+        break
+    if(new_distance_state != distance_state):
+        print(distance_state)
+        new_distance_state = distance_state
+    
+finish_timer = time.time()
+goal_time = round(finish_timer - start_timer, 2)
+# print('you won the game, congrats :) ')
+print('==============================')
+print('==============================')
+print('----------Your time: ---------')
+print(f'---------{goal_time} s...')
+print('==============================')
+print('===========EXITING============')
+print('==============================')
+print('=============GG===============')
+print('==============================')
+print('=======THX FOR THE GAME=======')
+print('==============================')
+print('==============================')
+
+
+
+"""
+# Drugi gracz
+player2 = PlayerPoint()
+(x2, y2) = player2.get_coords()
+print("Player 2")
+print(x2)
+print(y2)
+"""
+
